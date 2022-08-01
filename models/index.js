3cdf9e9f2cabb037c62a6d320026da5c54ae1350
GabrielWarner <gabe0412@uw.edu>
2022-08-01 10:25:13 -0700

    initial file creation and added code to server

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const Location = require("./location.js");
+const Trip = require("./trip.js");
+const Traveller = require("./traveller.js")
+
+
+
+module.exports = {
+    Location,
+    Trip,
+    Traveller
+}
